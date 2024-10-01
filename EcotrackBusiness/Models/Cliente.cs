b99b562c0c2c7de5d2a96c919c3111e8addd9656
MatheusCFBT <matheusterzini@gmail.com>
2024-09-30 23:19:52 -0300

    Setup data layer

---
+++ EcotrackBusiness/Models/Cliente.cs
@@ -0,0 +1,13 @@
+namespace EcotrackBusiness.Models
+{
+    public class Cliente : Entity 
+    {
+        public string? Nome { get; set; }
+
+        public string? Cpf { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+    }
+}
---
+++ EcotrackData/Context/EcotrackDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EcotrackBusiness.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecotrack.Context
+{
+    public class EcotrackDbContext : DbContext
+    {
+        public EcotrackDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(EcotrackDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
+
